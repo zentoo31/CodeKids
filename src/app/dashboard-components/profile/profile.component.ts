efe632bcc083209b2422427d299c0c8bb22868b8
zentoo31 <zentoo31@gmail.com>
2024-09-17 17:40:38 -0500

    17/09/24

--- src/app/dashboard-components/profile/profile.component.ts
+++ src/app/dashboard-components/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule} from "@angular/material/card"
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
